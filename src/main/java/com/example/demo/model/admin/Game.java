1ee4073d40ce7ecaff7aaff056898b2c10e82720
PeliPeli2 <harisinavi@gmail.com>
2021-06-07 13:33:22 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/model/admin/Game.java
@@ -0,0 +1,10 @@
+package com.example.demo.model.admin;
+import java.util.ArrayList;
+import  java.util.List;
+public class Game {
+    public String name;
+    public int id;
+    public boolean started;
+    public List<User> users = new ArrayList<>();
+
+}
